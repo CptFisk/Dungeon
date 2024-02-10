f843336ee9294772cd3d5d3d736ff52817b1c691
CptFisk <marcus@kammarfelt.eu>
2024-02-10 12:35:52 +0100

    Added scale.hpp

---
+++ include/utility/scale.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Utility {
+template<typename T>
+T
+Scale(const T& value, const T& inMin, const T& inMax, const T& outMin, const T& outMax) {
+    // Ensure the input value is within the original range
+    if (value < inMin)
+        return outMin;
+    if (value > inMax)
+        return outMax;
+
+    // Perform the scaling
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+}
