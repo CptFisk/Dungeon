23cdfe71a58e4473f237ec8d036f6ad659c4705a
CptFisk <marcus@kammarfelt.eu>
2024-02-12 14:13:41 +0100

    Rewrote trigonometry into template

--- include/utility/trigonometry.hpp
+++ include/utility/trigonometry.hpp
@@ -6,10 +6,16 @@ namespace Utility{
  * @brief Calculate the new position based on angle and velocity
  * @param angle Angle the object is moving. 0 degree is equal to 3 o'clock
  * @param velocity Velocity of object in pixels
- * @param x Reference to x-coord
- * @param y Reference to y-coord
  */
-void calculateVector(const float& angle, const float& velocity, float &x, float &y);
+template <typename T>
+std::pair<T,T> calculateVector(const T& angle, const T& velocity, const T&x, const T& y){
+    auto angleRadians = angle * M_PI / 180.0;
+
+    auto deltaX = velocity * cos(angleRadians);
+    auto deltaY = velocity * sin(angleRadians);
+    return std::make_pair(static_cast<T>(deltaX), static_cast<T>(deltaY));
+}
+
 
 /***
  * @brief Calculate the angle between 2 points in a coordinate system
@@ -17,4 +23,6 @@ void calculateVector(const float& angle, const float& velocity, float &x, float 
  */
 float calculateAngle(const std::pair<float,float>& p1,const std::pair<float,float>& p2);
 float calculateAngle(const float& x1, const float& y1, const float& x2, const float& y2);
+
+
 }
--- source/utility/trigonometry.cpp
+++ source/utility/trigonometry.cpp
@@ -2,15 +2,6 @@
 #include <utility/trigonometry.hpp>
 
 namespace Utility {
-void
-calculateVector(const float& angle, const float& velocity, float& x, float& y) {
-    auto angleRadians = angle * M_PI / 180.0;
-
-    auto deltaX = velocity * cos(angleRadians);
-    auto deltaY = velocity * sin(angleRadians);
-    x           = static_cast<float>(deltaX);
-    y           = static_cast<float>(deltaY);
-}
 
 float
 calculateAngle(const std::pair<float, float>& p1, const std::pair<float, float>& p2) {
