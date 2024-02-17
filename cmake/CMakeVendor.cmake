404d0efbc461ce52f41675b32edcc700ce51d118
CptFisk <marcus@kammarfelt.eu>
2024-02-17 09:14:04 +0100

    Splitted into 2 executables

--- cmake/CMakeVendor.cmake
+++ cmake/CMakeVendor.cmake
@@ -2,6 +2,7 @@ message(STATUS "Vendor libs")
 
 add_subdirectory(vendor/SDL ${CMAKE_BINARY_DIR}/SDL)
 add_subdirectory(vendor/SDL_image ${CMAKE_BINARY_DIR}/SDL_image)
+add_subdirectory(vendor/SDL_ttf ${CMAKE_BINARY_DIR}/SDL_ttf)
 add_subdirectory(vendor/nanosvg ${CMAKE_BINARY_DIR}/nanosvg)
 add_subdirectory(vendor/json)
 
