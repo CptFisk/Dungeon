156b7e838f9589b7c2bfe6c952c1daccbcba8091
CptFisk <marcus@kammarfelt.eu>
2024-02-18 00:31:45 +0100

    More change to level struct, also added a function to generate a pointer to it.

--- include/level/structures.hpp
+++ include/level/structures.hpp
@@ -1,15 +1,41 @@
 #pragma once
 #include <cstdint>
-namespace Level{
-    struct LevelHeader{
-        uint8_t HeaderVersion;
-        char MapName[31];
+namespace Level {
 
-        uint8_t BackgroundRed;
-        uint8_t BackgroundGreen;
-        uint8_t BackgroundBlue;
+enum TileType : uint8_t { Background, Obstacle };
 
-        uint8_t MapSizeX;
-        uint8_t MapSizeY;
-    };
+struct Tile {
+    TileType Type;
+    size_t   Graphic;
+};
+
+struct LevelHeader {
+    uint8_t HeaderVersion;
+    char    MapName[31];
+
+    uint8_t BackgroundRed;
+    uint8_t BackgroundGreen;
+    uint8_t BackgroundBlue;
+
+    uint8_t MapSizeX;
+    uint8_t MapSizeY;
+};
+
+struct Level {
+    LevelHeader Header;
+    TileType*   Tiles;
+};
+
+Level* getNewLevel(const uint8_t maxX, const uint8_t maxY){
+    Level* level = new Level;
+    level->Header.MapSizeX = maxX;
+    level->Header.MapSizeY = maxY;
+    size_t tiles = static_cast<size_t>(maxX) * static_cast<size_t>(maxY);
+    level->Tiles = new TileType[tiles];
+
+    return level;
+}
+
+
+
 }
