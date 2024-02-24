881541182bb8c50430fcbd91d0635f5338940f8c
CptFisk <marcus@kammarfelt.eu>
2024-02-24 11:55:14 +0100

    Added memory allocation

--- source/editor/header.cpp
+++ source/editor/header.cpp
@@ -23,6 +23,7 @@ Editor::uiHeader() {
                 mMapLoaded = true;
                 pTile = Level::newTileData(pLevelHeader->Level.SizeX, pLevelHeader->Level.SizeY);
                 pVisualTile = newVisualTile();  //Generate a new visual tile
+                pVisualTileType = newVisualTileType();
 
                 pLevelHeader->Color.BackgroundRed = static_cast<float>(color[0] * 255.0f);
                 pLevelHeader->Color.BackgroundGreen = static_cast<float>(color[1] * 255.0f);
