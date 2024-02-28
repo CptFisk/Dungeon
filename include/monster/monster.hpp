c9f1f140eacf83bdb85d62460113600af2821cb0
CptFisk <marcus@kammarfelt.eu>
2024-02-28 08:59:16 +0100

    Made some basic monster

--- include/monster/monster.hpp
+++ include/monster/monster.hpp
@@ -1,14 +1,39 @@
 #pragma once
+#include <common/structures.hpp>
+#include <global.hpp>
+#include <graphics/animatedTexture.hpp>
 
-namespace Monster{
-class BaseMonster{
+namespace Monster {
+class BaseMonster {
   public:
     BaseMonster(const int& health, const float& velocity);
+    BaseMonster(const BaseMonster& other);
     ~BaseMonster();
+
+    BaseMonster* spawn() const;
+
     bool damage(const int& damage);
+
+    void addAnimatedTexture(Objects::ObjectAction action, Directions direction, Graphics::AnimatedTexture* texture);
+
+    void setAction(Objects::ObjectAction action);
+    void setDirection(Directions direction);
+
   protected:
   private:
     const float mVelocity;
-    int mHealth;    //Monster health
+    int         mHealth; // Monster health
+
+    Objects::ObjectAction mAction;    // What are we doing
+    Directions            mDirection; // The direction we are facing
+
+    std::map<std::pair<Objects::ObjectAction, Directions>, Graphics::AnimatedTexture*> mTextures;
+
+    // Pointer that SDL_Render refer to
+    SDL_FRect    mMonsterPosition;
+    SDL_Texture* mCurrentTexture;
+    SDL_FRect*   mCurrentViewport;
+
+    void updateReferences();
 };
 }
--- source/monster/monster.cpp
+++ source/monster/monster.cpp
@@ -5,12 +5,49 @@ BaseMonster::BaseMonster(const int& health, const float& velocity)
   : mHealth(health)
   , mVelocity(velocity) {}
 
+BaseMonster::BaseMonster(const Monster::BaseMonster& other)
+  : mHealth(other.mHealth)
+  , mVelocity(other.mVelocity) {}
+
 BaseMonster::~BaseMonster() = default;
+
+BaseMonster*
+BaseMonster::spawn() const {
+    return new BaseMonster(*this);
+}
 
 bool
 BaseMonster::damage(const int& damage) {
-    if(mHealth -= damage <= 0)
+    if (mHealth -= damage <= 0)
         return true;
     return false;
 }
+
+void
+BaseMonster::addAnimatedTexture(Objects::ObjectAction action, Directions direction, Graphics::AnimatedTexture* texture) {
+    mTextures[{ action, direction }] = texture;
+    if (mCurrentTexture == nullptr || mCurrentViewport == nullptr) {
+        mCurrentTexture  = texture->mTexture;
+        mCurrentViewport = texture->getViewport();
+    }
+}
+
+void
+BaseMonster::setDirection(Directions direction) {
+    mDirection = direction;
+    updateReferences();
+}
+
+void
+BaseMonster::setAction(Objects::ObjectAction action) {
+    mAction = action;
+    updateReferences();
+}
+
+void
+BaseMonster::updateReferences() {
+    mCurrentTexture  = mTextures[{ mAction, mDirection }]->getTexture();
+    mCurrentViewport = mTextures[{ mAction, mDirection }]->getViewport();
+}
+
 }
