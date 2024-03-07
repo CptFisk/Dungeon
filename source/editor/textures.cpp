c4bef6eac45954df6973df1e62c10e67bd1e862f
CptFisk <marcus@kammarfelt.eu>
2024-03-07 20:12:07 +0100

    Working texture window

--- source/editor/textures.cpp
+++ source/editor/textures.cpp
@@ -4,10 +4,24 @@ namespace Editor{
 void
 Editor::uiTexture() {
     ImGui::ShowDemoWindow();
-    /*
+
     if (ImGui::Begin("Textures", &mWindowOpen["Textures"], ImGuiWindowFlags_AlwaysAutoResize)) {
+        static int currentItem;
+
+        std::vector<std::string> elements = {"Hello", "World"};
+        if(ImGui::BeginListBox("##Textures")){
+            for(int i = 0; i < elements.size(); i++){
+                const bool selected = (currentItem == i);
+                if(ImGui::Selectable(elements[i].c_str(), selected)){
+                    currentItem = i;
+                }
+                if(selected)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndListBox();
+        }
         ImGui::End();
     }
-    */
+
 }
 }
