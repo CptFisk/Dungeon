b6ac71e58c269534cc2676a6b070050de9b0f835
CptFisk <marcus@kammarfelt.eu>
2024-03-10 14:08:11 +0100

    Correcting errors in sdl.hpp / sdl.cpp

--- include/common/initializer/sdl.hpp
+++ include/common/initializer/sdl.hpp
@@ -1,18 +1,18 @@
 #pragma once
+#include <SDL3/SDL.h>
 #include <common/initializer/initializer.hpp>
 
-#include <SDL3/SDL.h>
+namespace Common {
+class SDLInitializer : public Initializer {
+  public:
+    SDLInitializer(SDL_Window** window, SDL_Renderer** render, std::string& title);
+    void startup() override;
+    void shutdown() override;
 
-namespace Common{
-    class SDLInitializer : public Initializer{
-      public:
-        SDLInitializer(SDL_Window** window, SDL_Renderer** render, const std::string& title);
-        void startup();
-        void shutdown();
-      private:
-      protected:
-        SDL_Window** pWindow;
-        SDL_Renderer** pRenderer;
-        const std::string mTitle;
-    };
+  private:
+  protected:
+    SDL_Window**      pWindow;
+    SDL_Renderer**    pRenderer;
+    const std::string mTitle;
+};
 }
