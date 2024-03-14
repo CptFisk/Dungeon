ed4dc51bcf171314c96d3a1f24bb706821a33c92
CptFisk <marcus@kammarfelt.eu>
2024-03-14 20:18:38 +0100

    Re-work of assets

--- source/editor/assets.cpp
+++ source/editor/assets.cpp
@@ -9,16 +9,16 @@ Editor::uiAssets() {
 
     if (ImGui::Begin("Assets", &mWindowOpen["Assets"], ImGuiWindowFlags_AlwaysAutoResize)) {
         ImGui::Text("Assets");
-        for(auto& element : pAssets.Assets){
+        for(auto& element : fileAssets.Assets){
             ImGui::InputText("##", &element);
         }
 
         ImGui::InputText("##asset", &mStringInput);
         ImGui::SameLine();
         if (ImGui::Button("Add")) {
-            const auto exist = std::find(pAssets.Assets.begin(), pAssets.Assets.end(), mStringInput);
-            if(exist == pAssets.Assets.end()) {
-                Level::addAsset(mStringInput, pAssets);
+            const auto exist = std::find(fileAssets.Assets.begin(), fileAssets.Assets.end(), mStringInput);
+            if(exist == fileAssets.Assets.end()) {
+                Level::File::addAsset(mStringInput, fileAssets);
                 mStringInput.clear();
             }
         }
