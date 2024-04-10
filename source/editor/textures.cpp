28fe4820a542a5f09953240f52801bdfac7dcfa7
CptFisk <marcus@kammarfelt.eu>
2024-04-10 20:47:18 +0200

    Added filter to textures

--- source/editor/textures.cpp
+++ source/editor/textures.cpp
@@ -1,20 +1,24 @@
 #include <editor/editor.hpp>
+#include <misc/cpp/imgui_stdlib.h>
 
 namespace Editor{
 void
 Editor::uiTexture() {
     if (ImGui::Begin("Textures", &mWindowOpen["Textures"], ImGuiWindowFlags_AlwaysAutoResize)) {
         static int currentItem;
-
+        static std::string filter;
+        ImGui::InputText("##Filter", &filter);
         if(ImGui::BeginListBox("##Textures")){
             for(int i = 0; i < mTextures.size(); i++){
-                const bool selected = (currentItem == i);
-                if(ImGui::Selectable(mTextures[i].c_str(), selected)){
-                    currentItem = i;
-                    mSelectedTexture = mTextures[i];
+                if(mTextures[i].find(filter) != std::string::npos) {
+                    const bool selected = (currentItem == i);
+                    if (ImGui::Selectable(mTextures[i].c_str(), selected)) {
+                        currentItem      = i;
+                        mSelectedTexture = mTextures[i];
+                    }
+                    if (selected)
+                        ImGui::SetItemDefaultFocus();
                 }
-                if(selected)
-                    ImGui::SetItemDefaultFocus();
             }
             ImGui::EndListBox();
         }
