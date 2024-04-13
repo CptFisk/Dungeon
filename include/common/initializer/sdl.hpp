cd36bdb7ab9504a5251b851b216561985497f9a2
CptFisk <marcus@kammarfelt.eu>
2024-04-13 08:47:16 +0200

    Added the option to pass dimensions to SDL

--- include/common/initializer/sdl.hpp
+++ include/common/initializer/sdl.hpp
@@ -1,18 +1,28 @@
 #pragma once
+#include <SDL3/SDL.h>
 #include <common/initializer/initializer.hpp>
 
-#include <SDL3/SDL.h>
+namespace Common {
+class SDLInitializer : public Initializer {
+  public:
+    /**
+     * @brief Initialize a new SDL instance
+     * @param window Window pointer
+     * @param render Renderer pointer
+     * @param w Window width
+     * @param h Window height
+     * @param title Title
+     */
+    SDLInitializer(SDL_Window** window, SDL_Renderer** render, const int& w, const int& h, std::string title);
+    void startup() override;
+    void shutdown() override;
 
-namespace Common{
-    class SDLInitializer : public Initializer{
-      public:
-        SDLInitializer(SDL_Window** window, SDL_Renderer** render, std::string title);
-        void startup() override;
-        void shutdown() override;
-      private:
-      protected:
-        SDL_Window** pWindow;
-        SDL_Renderer** pRenderer;
-        const std::string mTitle;
-    };
+  private:
+  protected:
+    const int         width;  // Requested window width
+    const int         height; // Requested window height
+    SDL_Window**      window;
+    SDL_Renderer**    renderer;
+    const std::string windowTitle;
+};
 }
--- source/common/initializer/sdl.cpp
+++ source/common/initializer/sdl.cpp
@@ -1,10 +1,15 @@
-#include <common/initializer/sdl.hpp>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
+#include <common/initializer/sdl.hpp>
 #include <iostream>
 
-namespace Common{
-SDLInitializer::SDLInitializer(SDL_Window** window, SDL_Renderer** render, std::string title) :pWindow(window), pRenderer(render), mTitle(std::move(title)){}
+namespace Common {
+SDLInitializer::SDLInitializer(SDL_Window** window, SDL_Renderer** render, const int& w, const int& h, std::string title)
+  : window(window)
+  , renderer(render)
+  , width(w)
+  , height(h)
+  , windowTitle(std::move(title)) {}
 
 void
 SDLInitializer::startup() {
@@ -17,13 +22,13 @@ SDLInitializer::startup() {
     renderFlags |= SDL_RENDERER_ACCELERATED;
     renderFlags |= SDL_RENDERER_PRESENTVSYNC;
 
-    *pWindow   = SDL_CreateWindow(mTitle.c_str(), 800, 600, windowFlags);
-    *pRenderer = SDL_CreateRenderer(*pWindow, nullptr, renderFlags);
+    *window   = SDL_CreateWindow(windowTitle.c_str(), width, height, windowFlags);
+    *renderer = SDL_CreateRenderer(*window, nullptr, renderFlags);
 }
 
 void
 SDLInitializer::shutdown() {
-    SDL_DestroyRenderer(*pRenderer);
-    SDL_DestroyWindow(*pWindow);
+    SDL_DestroyRenderer(*renderer);
+    SDL_DestroyWindow(*window);
 }
 }
