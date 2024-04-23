10c3927e8ec340ada053f29928bb3e51b5697a0f
CptFisk <marcus@kammarfelt.eu>
2024-04-23 21:17:38 +0200

    Added function to list all folders

--- include/utility/file.hpp
+++ include/utility/file.hpp
@@ -2,10 +2,30 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <vector>
 
 namespace Utility {
+    /**
+     * @brief Read the data from a file
+     * @param filePath Path to the file
+     * @return Contents as a string
+     */
     std::string getFileContent(const std::string &filePath);
 
+    /**
+     * @brief Returns a list of all the folders inside a folder
+     * @param rootFolder
+     * @return Vector with all folders
+     */
+    std::vector<std::filesystem::path>
+    getFolders(const std::string& rootFolder);
+
+    /**
+     * @brief Return all the files with the correct extension from a folder
+     * @param folderPath Path to folder
+     * @param extension Extension (.json, .txt)
+     * @return Vector with all files matching the criteria
+     */
     std::vector<std::filesystem::path>
     getFiles(const std::string& folderPath, const std::string& extension);
 }
