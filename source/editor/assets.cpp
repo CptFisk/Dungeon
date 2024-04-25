8ce5701d3c565f2e0748bc6ea9bb89c0476b54f1
CptFisk <marcus@kammarfelt.eu>
2024-04-25 14:44:53 +0200

    Added remove button

--- source/editor/assets.cpp
+++ source/editor/assets.cpp
@@ -1,7 +1,7 @@
+#include <algorithm>
 #include <editor/editor.hpp>
 #include <imgui.h>
 #include <misc/cpp/imgui_stdlib.h>
-#include <algorithm>
 
 namespace Editor {
 void
@@ -9,19 +9,13 @@ Editor::uiAssets() {
 
     if (ImGui::Begin("Assets", &mWindowOpen["Assets"], ImGuiWindowFlags_AlwaysAutoResize)) {
         ImGui::Text("Assets");
-        for(auto& element : fileAssets.Assets){
+        for (auto& element : fileAssets.Assets) {
             ImGui::InputText("##", &element, ImGuiInputTextFlags_ReadOnly);
             ImGui::SameLine();
-            if(ImGui::Button(("Delete##" + element).c_str())){
-                for(auto it = fileAssets.Assets.begin(); it != fileAssets.Assets.end(); ++it){
-                    if(*it == element){
-                        fileAssets.Assets.erase(it);
-                        break;  //Stop the loop
-                    }
-                }
+            if (ImGui::Button(("Delete##" + element).c_str())) {
+                Level::File::removeAsset(element, fileAssets,editorTiles);
             }
         }
-
     }
     mWindows["Assets"] = { ImGui::GetWindowPos(), ImGui::GetWindowSize() };
     ImGui::End();
