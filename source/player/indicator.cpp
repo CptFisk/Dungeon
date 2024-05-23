8b3ace9de9fc8e0fb3c1c64a44d4ef5764e26537
CptFisk <marcus@kammarfelt.eu>
2024-05-23 15:34:27 +0200

    Migrating from SDL3 to SDL2

--- source/player/indicator.cpp
+++ source/player/indicator.cpp
@@ -13,7 +13,7 @@ Indicator::Indicator(bool&                              visible,
   , mValue(value)
   , pRenderer(renderer)
   , pIconTexture(texture)
-  , mNumbers(std::move(numbers))
+  , mNumbers(numbers)
   , mPositionIcon(8.0f, 192 - distance, 16.0f, 16.0f)
   , mPositionNumber({ 28.0f, (192 - distance) + 4.0f, 8.0f, 8.0f },
                     { 36.0f, (192 - distance) + 4.0f, 8.0f, 8.0f },
@@ -23,13 +23,12 @@ void
 Indicator::draw() {
     if (mVisible) {
         // Icon
-        //SDL_RenderTexture(pRenderer, pIconTexture->getTexture(), pIconTexture->getViewport(), &mPositionIcon);
+        SDL_RenderCopyF(pRenderer, pIconTexture->getTexture(), pIconTexture->getViewport(), &mPositionIcon);
         // Numbers
         int pos = 0;
         if (mValue > 0 && mValue < 999) {
-            for (const auto& n : Utility::splitNumbers(mValue)) {
-                // SDL_RenderTexture(pRenderer, mNumbers.Texture, &mNumbers.getView(n), &mPositionNumber[pos++]);
-            }
+            for (const auto& n : Utility::splitNumbers(mValue))
+                SDL_RenderCopyF(pRenderer, mNumbers.Texture, &mNumbers.getView(n), &mPositionNumber[pos++]);
         }
     }
 }
