3a4b00309e01150061203c0337f70adb074618a1
CptFisk <marcus@kammarfelt.eu>
2024-05-24 23:49:08 +0200

    Door class prototype working

--- include/objects/door.hpp
+++ include/objects/door.hpp
@@ -1,23 +1,39 @@
 #pragma once
+#include <SDL.h>
 #include <common/structures.hpp>
 #include <graphics/animatedTexture.hpp>
 #include <graphics/types/simpleTexture.hpp>
+#include <mutex>
+#include <thread>
 
 namespace Objects {
 
 class Door {
   public:
-    Door(const Graphics::typeSimpleTexture& graphic, const Graphics::AnimatedTexture& animatedGraphic, bool open = false);
+    Door(const SDL_FRect&             position,
+         Graphics::typeSimpleTexture& graphic,
+         Graphics::AnimatedTexture*   opening,
+         Graphics::AnimatedTexture*   closing,
+         bool                         open = false);
     ~Door();
 
     [[nodiscard]] Common::typeDrawData& getDrawData();
-    void open(bool condition);
+    void                                interact(bool condition);
+
   protected:
+    void setOpenGraphic();
+    void setClosedGraphic();
+
   private:
     bool                        mOpen; // State of door (false = closed, true = open)
-    Graphics::AnimatedTexture   mAnimatedGraphic;
+    Graphics::AnimatedTexture*  mAnimationOpening;
+    Graphics::AnimatedTexture*  mAnimationClosing;
     Graphics::typeSimpleTexture mGraphic;
 
     Common::typeDrawData mDrawData; // Used for returning the graphic
+    SDL_FRect            mPosition;
+
+    std::mutex           mtx;
+    std::thread          mThread; // Used to swap state after animation finish
 };
 }
--- source/objects/door.cpp
+++ source/objects/door.cpp
@@ -1,25 +1,76 @@
+#include <iostream>
 #include <objects/door.hpp>
 
 namespace Objects {
 
-Door::Door(const Graphics::typeSimpleTexture& graphic, const Graphics::AnimatedTexture& animatedGraphic, bool open)
-  : mOpen(open)
+Door::Door(const SDL_FRect&             position,
+           Graphics::typeSimpleTexture& graphic,
+           Graphics::AnimatedTexture*   opening,
+           Graphics::AnimatedTexture*   closing,
+           bool                         open)
+  : mPosition(position)
+  , mOpen(open)
   , mGraphic(graphic)
-  , mAnimatedGraphic(animatedGraphic) {
-
-
+  , mAnimationOpening(opening)
+  , mAnimationClosing(closing) {
+    mDrawData.Position = &mPosition;
+    if (open) {
+        mDrawData.Viewport = closing->getViewport();
+        mDrawData.Texture  = closing->getTexture();
+    } else {
+        mDrawData.Viewport = opening->getViewport();
+        mDrawData.Texture  = opening->getTexture();
+    }
 }
 
-Door::~Door() {}
+Door::~Door() {
+    mThread.join();
+}
 
 void
-Door::open(bool condition) {
-    mOpen = !mOpen && condition;
+Door::interact(bool condition) {
+    bool prevState = mOpen;
+    mOpen          = !mOpen && condition;
+    if (prevState != mOpen) {
+        // Detect change in door
+        if (mOpen) {
+            mAnimationOpening->runCycles(1);
+            if (mThread.joinable())
+                mThread.join();
+            mThread = std::thread([&]() {
+                std::this_thread::sleep_for(std::chrono::milliseconds(mAnimationOpening->getTicks() * 10));
+                setOpenGraphic();
+            });
+        } else {
+            mAnimationClosing->runCycles(1);
+            if (mThread.joinable())
+                mThread.join();
+
+            mThread = std::thread([&]() {
+                std::this_thread::sleep_for(std::chrono::milliseconds(mAnimationClosing->getTicks() * 10));
+                setClosedGraphic();
+            });
+        }
+    }
+}
+
+void
+Door::setOpenGraphic() {
+    std::lock_guard<std::mutex> guard(mtx);
+    mDrawData.Texture  = mAnimationClosing->getTexture();
+    mDrawData.Viewport = mAnimationClosing->getViewport();
+}
+
+void
+Door::setClosedGraphic() {
+    std::lock_guard<std::mutex> guard(mtx);
+    mDrawData.Texture  = mAnimationOpening->getTexture();
+    mDrawData.Viewport = mAnimationOpening->getViewport();
 }
 
 Common::typeDrawData&
 Door::getDrawData() {
+    std::lock_guard<std::mutex> guard(mtx);
     return mDrawData;
 }
-
 }
