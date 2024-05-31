42b95665f7a76397f070a5b6bfcf62101ca6d843
CptFisk <marcus@kammarfelt.eu>
2024-05-31 10:19:17 +0200

    Added constants for animations

--- include/level/types/doors.hpp
+++ include/level/types/doors.hpp
@@ -4,18 +4,23 @@
 
 namespace Level::File {
 struct typeDoorsData {
-    uint8_t     X;         // X-coordinate
-    uint8_t     Y;         // Y-coordinate
-    std::string Condition; // Condition for open door
+    uint8_t     X;              // X-coordinate
+    uint8_t     Y;              // Y-coordinate
+    std::string Condition;      // Condition for open door
+    std::string GraphicOpen;    // Graphic used for opening door
+    std::string GraphicClosing; // Graphic used for closing door
 
     typeDoorsData()
       : X{}
       , Y{}
-      , Condition{} {}
+      , Condition{}
+      , GraphicOpen("DoorOpenAnimation")
+      , GraphicClosing("DoorCloseAnimation") {}
     typeDoorsData(const uint8_t& x, const uint8_t& y, const std::string& condition)
       : X(x)
       , Y(y)
-      , Condition(condition) {}
+      , GraphicOpen("DoorOpenAnimation")
+      , GraphicClosing("DoorCloseAnimation") {}
 };
 
 struct typeDoors {
