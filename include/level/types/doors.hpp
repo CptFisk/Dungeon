64e23f05ad3caaf7b00bd53e46a9d4716bf27d65
CptFisk <marcus@kammarfelt.eu>
2024-05-31 23:23:54 +0200

    New data type

--- include/level/types/doors.hpp
+++ include/level/types/doors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace Level::File {
 struct typeDoorsData {
---
+++ include/level/types/warps.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <cstdint>
+
+namespace Level::File{
+struct typeWarpData{
+    uint8_t     X;              // X-coordinate
+    uint8_t     Y;              // Y-coordinate
+};
+}
