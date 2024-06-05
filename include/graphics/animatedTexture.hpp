df47f30cc415b31539b850ef7a2261b9f0cad2ba
CptFisk <marcus@kammarfelt.eu>
2024-06-05 14:22:27 +0200

    Updated hpp

--- include/graphics/animatedTexture.hpp
+++ include/graphics/animatedTexture.hpp
@@ -9,15 +9,15 @@ class AnimatedTexture {
     AnimatedTexture(SDL_Texture* texture, const int& ticks, const bool& paused);
     ~AnimatedTexture();
 
-    [[nodiscard]] SDL_Texture*& getTexture();
-    SDL_Rect*                   getViewport();
-    SDL_Rect*                   getViewport(const int& pos);
-    void                        addViewport(const SDL_Rect& view);
-    void                        updateTexture();
-    void                        runCycles(const int& cycles);
-    std::vector<SDL_Rect>       getViewports();    // Get all the viewports for the animations
-    [[nodiscard]] bool          done() const;      // Animation is last frame
-    [[nodiscard]] int           getTicks() const;  // Return the maximum number of ticks
+    [[nodiscard]] SDL_Texture*&         getTexture();
+    [[nodiscard]] SDL_Rect*             getViewport();
+    [[nodiscard]] SDL_Rect*             getViewport(const int& pos);
+    void                                addViewport(const SDL_Rect& view);
+    void                                updateTexture();
+    void                                runCycles(const int& cycles);
+    [[nodiscard]] std::vector<SDL_Rect> getViewports();   // Get all the viewports for the animations
+    [[nodiscard]] bool                  done() const;     // Animation is last frame
+    [[nodiscard]] int                   getTicks() const; // Return the maximum number of ticks
 
   private:
   protected:
