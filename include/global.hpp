761eab5382bfcb5f8757c217820e36213c49d155
CptFisk <marcus@kammarfelt.eu>
2024-06-17 22:43:35 +0200

    Added macros for converting data

--- include/global.hpp
+++ include/global.hpp
@@ -2,7 +2,8 @@
 #include <cstdint>
 
 // Casting macros
-#define UINT8(x) static_cast<uint8_t>(x)
-#define FLOAT(x) static_cast<uint8_t>(x)
-#define INT(x)   static_cast<int>(x)
+#define UINT8(x)  static_cast<uint8_t>(x)
+#define UINT16(x) static_cast<uint16_t>(x)
+#define FLOAT(x)  static_cast<float>(x)
+#define INT(x)    static_cast<int>(x)
 enum Directions : uint8_t { NORTH = 1 << 0, EAST = 1 << 1, SOUTH = 1 << 2, WEST = 1 << 3, ALL = NORTH | EAST | SOUTH | WEST };
