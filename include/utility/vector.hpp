24fcc826066a62fc4f6d5a8d45656b47ab93b8be
CptFisk <marcus@kammarfelt.eu>
2024-07-09 14:25:59 +0200

    Missing &

--- include/utility/vector.hpp
+++ include/utility/vector.hpp
@@ -4,7 +4,7 @@
 
 namespace Utility{
 template <typename T, typename Condition>
-bool removeElementInVector(std::vector<T> vector, Condition con){
+bool removeElementInVector(std::vector<T>& vector, Condition con){
     const auto size = vector.size();
     vector.erase(std::remove_if(vector.begin(), vector.end(), con), vector.end());
     return size != vector.size();
