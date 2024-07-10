31b7deb54039d7d78eee76a72385d2bfd76236f4
CptFisk <marcus@kammarfelt.eu>
2024-07-10 19:04:23 +0200

    Added function to cast coordinate to string

--- include/level/types/coordinate.hpp
+++ include/level/types/coordinate.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstdint>
+#include <string>
+#include <sstream>
+#include <global.hpp>
 
 namespace Level::File{
 struct typeMapCoordinate{
@@ -9,6 +12,16 @@ struct typeMapCoordinate{
 
     bool operator==(const typeMapCoordinate& other) const{
         return other.X == X && other.Y == Y && other.Z == Z;
+    }
+
+    bool operator !=(const typeMapCoordinate& other) const{
+        return other.X != X || other.Y != Y || other.Z != Z;
+    }
+
+    std::string toString() const{
+        std::ostringstream oss;
+        oss << INT(X) << INT(Y) << INT(Z);
+        return oss.str();
     }
 };
 }
