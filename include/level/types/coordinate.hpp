f29b18fc4c4dd86cf6b5583d84d318b84c86ec4d
CptFisk <marcus@kammarfelt.eu>
2024-07-10 22:11:42 +0200

    Added 2D coordinates

--- include/level/types/coordinate.hpp
+++ include/level/types/coordinate.hpp
@@ -5,28 +5,51 @@
 #include <string>
 
 namespace Level::File {
-struct typeMapCoordinate {
+struct type3DMapCoordinate {
     uint8_t X;
     uint8_t Y;
     uint8_t Z;
 
-    typeMapCoordinate()
+    type3DMapCoordinate()
       : X(0)
       , Y(0)
       , Z(0) {}
 
-    typeMapCoordinate(const uint8_t& x, const uint8_t& y, const uint8_t& z)
+    type3DMapCoordinate(const uint8_t& x, const uint8_t& y, const uint8_t& z)
       : X(x)
       , Y(y)
       , Z(z) {}
 
-    bool operator==(const typeMapCoordinate& other) const { return other.X == X && other.Y == Y && other.Z == Z; }
+    bool operator==(const type3DMapCoordinate& other) const { return other.X == X && other.Y == Y && other.Z == Z; }
 
-    bool operator!=(const typeMapCoordinate& other) const { return other.X != X || other.Y != Y || other.Z != Z; }
+    bool operator!=(const type3DMapCoordinate& other) const { return other.X != X || other.Y != Y || other.Z != Z; }
 
     [[nodiscard]] std::string toString() const {
         std::ostringstream oss;
         oss << INT(X) << INT(Y) << INT(Z);
+        return oss.str();
+    }
+};
+
+struct type2DMapCoordinate {
+    uint8_t X;
+    uint8_t Y;
+
+    type2DMapCoordinate()
+      : X(0)
+      , Y(0) {}
+
+    type2DMapCoordinate(const uint8_t& x, const uint8_t& y)
+      : X(x)
+      , Y(y) {}
+
+    bool operator==(const type2DMapCoordinate& other) const { return other.X == X && other.Y == Y; }
+
+    bool operator!=(const type2DMapCoordinate& other) const { return other.X != X || other.Y != Y; }
+
+    [[nodiscard]] std::string toString() const {
+        std::ostringstream oss;
+        oss << INT(X) << INT(Y);
         return oss.str();
     }
 };
