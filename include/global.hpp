4ddbb920d94e7ba2e6f4906d928c553b12687a53
CptFisk <marcus@kammarfelt.eu>
2024-07-26 08:44:51 +0200

    New constants and bit-functions

--- include/global.hpp
+++ include/global.hpp
@@ -15,16 +15,8 @@ constexpr int MAP_SIZE  = MAP_WIDTH * MAP_WIDTH;
 
 const int               LIGHT_ANIMATION_FRAMES = 7;          // Constant for how many frames a light-effect exist of
 const unsigned long int LIGHT_BITS             = 0xFF000000; // Used to mask out bit 24-31
-const unsigned long int LIGHT_CIRCLE = 0x0;
-const unsigned long int LIGHT_SQUARE = 0x0;
-
-const unsigned long int LIGHT_RED = 0x0;
-const unsigned long int LIGHT_YELLOW = 0x0;
-const unsigned long int LIGHT_WHITE = 0x0;
-
-const unsigned long int LIGHT_SMALL = 0x0;
-const unsigned long int LIGHT_MEDIUM = 0x0;
-const unsigned long int LIGHT_BIG = 0x0;
-
+const unsigned long int LIGHT_COLOUR           = 0x7000000;
+const unsigned long int LIGHT_SHAPE            = 0x18000000;
+const unsigned long int LIGHT_SIZE             = 0xE0000000;
 
 enum Directions : uint8_t { NORTH = 1 << 0, EAST = 1 << 1, SOUTH = 1 << 2, WEST = 1 << 3, ALL = NORTH | EAST | SOUTH | WEST };
--- include/utility/bits.hpp
+++ include/utility/bits.hpp
@@ -1,15 +1,29 @@
 #pragma once
 #include <bitset>
 
-namespace Utility{
+namespace Utility {
 template<std::size_t N>
 constexpr bool
-isAnyBitSet(const std::bitset<N>& bitset, const std::bitset<N> mask){
+isAnyBitSet(const std::bitset<N>& bitset, const std::bitset<N>& mask) {
     return (bitset & mask).any();
 }
 
 template<std::size_t N>
-constexpr void resetBits(std::bitset<N>& bitset, const std::bitset<N> mask){
+constexpr void
+resetBits(std::bitset<N>& bitset, const std::bitset<N>& mask) {
     bitset &= ~mask;
+}
+
+template<std::size_t N>
+constexpr int
+getSetBit(const std::bitset<N>& bitset, const std::bitset<N>& mask) {
+    std::bitset<N> data = bitset & mask;
+    if (data.count() != 1)
+        return 0;
+    for (int i = 0; i < N; ++i) {
+        if (data.test(i))
+            return i;
+    }
+    return 0;
 }
 }
