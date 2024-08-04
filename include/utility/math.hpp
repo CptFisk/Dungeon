e3d4100017071d5b955cffe0422538570cfdbe8b
CptFisk <marcus@kammarfelt.eu>
2024-08-04 15:03:36 +0200

    Added comments

--- include/utility/math.hpp
+++ include/utility/math.hpp
@@ -1,7 +1,14 @@
 #pragma once
-#include <vector>
 #include <algorithm>
+#include <vector>
 
-namespace Utility{
-    std::vector<int> splitNumbers(int n);
+namespace Utility {
+/**
+ * @brief Split a number into a vector.
+ * @param n Number to be split
+ * @return Vector with the same size as numbers of digits
+ * @example splitNumbers(123) will return a vector<int>{1,2,3}
+ */
+std::vector<int>
+splitNumbers(int n);
 }
