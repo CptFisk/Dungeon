c6f142f090c9179a123967189877bbac73038e70
CptFisk <marcus@kammarfelt.eu>
2024-08-11 09:47:51 +0200

    Typo

--- include/utility/string.hpp
+++ include/utility/string.hpp
@@ -1,19 +1,20 @@
 #pragma once
-#include <string>
 #include <cctype>
+#include <string>
+
 
 namespace Utility {
 
 /**
  * @brief Check if a character is equal to a-Z or A-Z.
- * 
+ *
  * @param c Character to check
- * @return true 
- * @return false 
+ * @return true
+ * @return false
  */
 constexpr bool
 isCharacter(const char& c) {
     const int v = static_cast<int>(toupper(c));
-    return (v >= 65 && v <= 90) : true ? false;
+    return (v >= 65 && v <= 90) ? true : false;
 }
 }
