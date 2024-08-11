178a09fbed42a5c6c9c2c22dedbacb8f3b5ea359
CptFisk <marcus@kammarfelt.eu>
2024-08-11 09:54:55 +0200

    Added function to get value from character

--- include/utility/string.hpp
+++ include/utility/string.hpp
@@ -2,7 +2,6 @@
 #include <cctype>
 #include <string>
 
-
 namespace Utility {
 
 /**
@@ -16,5 +15,20 @@ constexpr bool
 isCharacter(const char& c) {
     const int v = static_cast<int>(toupper(c));
     return (v >= 65 && v <= 90) ? true : false;
+}
+
+/**
+ * @brief Return the value of a character. Aa = 0, Bb = 1 and so on.
+ * @brief If the character is illegal it will return -1
+ *
+ * @param c Character
+ * @return constexpr int
+ */
+constexpr int
+charValue(const char& c) {
+    if (!isCharacter(c))
+        return -1;
+    const int v = static_cast<int>(toupper(c)) - 65;
+    return v;
 }
 }
