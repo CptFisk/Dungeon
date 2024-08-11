7710dd104b7da373b32c482f2fe957038e34dad6
CptFisk <marcus@kammarfelt.eu>
2024-08-11 18:15:44 +0200

    Added option for fullscreen

--- include/common/initializer/sdl.hpp
+++ include/common/initializer/sdl.hpp
@@ -1,7 +1,7 @@
 #pragma once
-#include <string>
 #include <SDL.h>
 #include <common/initializer/initializer.hpp>
+#include <string>
 
 namespace Common {
 class SDLInitializer : public Initializer {
@@ -12,16 +12,18 @@ class SDLInitializer : public Initializer {
      * @param render Renderer pointer
      * @param w Window width
      * @param h Window height
+     * @param fullscreen True for fullscreen
      * @param title Title
      */
-    SDLInitializer(SDL_Window*& window, SDL_Renderer*& render, const int& w, const int& h, std::string title);
+    SDLInitializer(SDL_Window*& window, SDL_Renderer*& render, const int& w, const int& h, const bool& fullscreen, std::string title);
     void startup() override;
     void shutdown() override;
 
   private:
   protected:
-    const int         width;  // Requested window width
-    const int         height; // Requested window height
+    const int         width;      // Requested window width
+    const int         height;     // Requested window height
+    const bool        fullscreen; // Requested mode
     SDL_Window*&      window;
     SDL_Renderer*&    renderer;
     const std::string windowTitle;
--- source/common/initializer/sdl.cpp
+++ source/common/initializer/sdl.cpp
@@ -1,13 +1,19 @@
+#include <SDL_image.h>
 #include <common/initializer/sdl.hpp>
 #include <iostream>
-#include <SDL_image.h>
 
 namespace Common {
-SDLInitializer::SDLInitializer(SDL_Window*& window, SDL_Renderer*& render, const int& w, const int& h, std::string title)
+SDLInitializer::SDLInitializer(SDL_Window*&   window,
+                               SDL_Renderer*& render,
+                               const int&     w,
+                               const int&     h,
+                               const bool&    fullscreen,
+                               std::string    title)
   : window(window)
   , renderer(render)
   , width(w)
   , height(h)
+  , fullscreen(fullscreen)
   , windowTitle(std::move(title)) {}
 
 void
@@ -16,10 +22,10 @@ SDLInitializer::startup() {
     IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
     Uint32 windowFlags = 0;
     Uint32 renderFlags = 0;
-    windowFlags |= SDL_WINDOW_RESIZABLE;
+    windowFlags |= fullscreen == true ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_RESIZABLE;
     renderFlags |= SDL_RENDERER_PRESENTVSYNC;
 
-    window   = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, width, height, windowFlags);
+    window   = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, windowFlags);
     renderer = SDL_CreateRenderer(window, -1, renderFlags);
 }
 
