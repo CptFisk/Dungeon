6e8615016eb1516218688c0ac67d074e12780e86
CptFisk <marcus@kammarfelt.eu>
2024-09-08 21:41:59 +0200

    ui scales

--- source/editor/grid.cpp
+++ source/editor/grid.cpp
@@ -7,21 +7,19 @@ Editor::uiDrawGrid() {
         return;
     // Set line color
     SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    const auto offsetX = static_cast<int>(mOffset.X);
-    const auto offsetY = static_cast<int>(mOffset.Y);
-    const auto tileX   = 16.0f * mScale.factorX;
-    const auto tileY   = 16.0f * mScale.factorY;
+    const auto tileX   = 16.0f * mScale.selectedScale;
+    const auto tileY   = 16.0f * mScale.selectedScale;
     const auto moduloX = static_cast<float>(fmod(mOffset.X, tileX));
     const auto moduloY = static_cast<float>(fmod(mOffset.Y, tileY));
     // Draw horizontal lines
     for (int i = 0; i <= 12; i++) {
         const auto y = static_cast<float>(i) * tileY + moduloY;
-        SDL_RenderDrawLineF(pRenderer, 0.0f, y, mScale.windowWidth, y);
+        SDL_RenderDrawLineF(pRenderer, 0.0f, y, mScale.windowWidthF, y);
     }
     // Draw vertical lines
     for (int i = 0; i <= 16; i++) {
         const auto x = static_cast<float>(i) * tileX + moduloX;
-        SDL_RenderDrawLineF(pRenderer, x, 0.0f, x, mScale.windowWidth);
+        SDL_RenderDrawLineF(pRenderer, x, 0.0f, x, mScale.windowWidthF);
     }
 }
 }
