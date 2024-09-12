dfd8a45406fc932844dc8818be47bc9482305ebd
CptFisk <marcus@kammarfelt.eu>
2024-09-12 21:12:23 +0200

    Removed constexpr

--- include/editor/utility/mouse.hpp
+++ include/editor/utility/mouse.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <imgui.h>
-#include <level/types/tile.hpp>
 #include <string>
 namespace Editor {
 
@@ -30,35 +29,8 @@ enum Mouse {
  * @param mouse Current mouse state
  * @return Colour code in format of #XXXXXX
  */
-[[nodiscard]] constexpr std::string
-getMouseColorCode(const Mouse& mouse) {
-    switch (mouse) {
-        case Mouse::DEFAULT:
-        case Mouse::CLEAR_ALL:
-        case Mouse::TEXTURE:
-            return ""; // They don't have a colour code
-        case Mouse::UP:
-            return "#C0E6F5";
-        case Mouse::DOWN:
-            return "#FFFF66";
-        case Mouse::WALL:
-            return "#D86DCD";
-        case Mouse::OBSTACLE:
-            return "#BE5014";
-        case Mouse::SPARE:
-            return "#E8E8E8";
-        case Mouse::DOOR:
-            return "#000000";
-        case Mouse::WARP:
-            return "#8ED973";
-        case Mouse::TOP_LAYER:
-            return "#8960CF";
-        case Mouse::LIGHTNING:
-            return "#C8BFE7";
-        default:
-            return "";
-    }
-}
+[[nodiscard]] std::string
+getMouseColorCode(const Mouse& mouse);
 
 template<typename T>
 bool
---
+++ source/editor/utility/mouse.cpp
@@ -0,0 +1,33 @@
+#include <editor/utility/mouse.hpp>
+
+namespace Editor{
+std::string
+getMouseColorCode(const Mouse& mouse){
+    switch (mouse) {
+        case Mouse::DEFAULT:
+        case Mouse::CLEAR_ALL:
+        case Mouse::TEXTURE:
+            return ""; // They don't have a colour code
+        case Mouse::UP:
+            return "#C0E6F5";
+        case Mouse::DOWN:
+            return "#FFFF66";
+        case Mouse::WALL:
+            return "#D86DCD";
+        case Mouse::OBSTACLE:
+            return "#BE5014";
+        case Mouse::SPARE:
+            return "#E8E8E8";
+        case Mouse::DOOR:
+            return "#000000";
+        case Mouse::WARP:
+            return "#8ED973";
+        case Mouse::TOP_LAYER:
+            return "#8960CF";
+        case Mouse::LIGHTNING:
+            return "#C8BFE7";
+        default:
+            return "";
+    }
+}
+}
