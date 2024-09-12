101e3de474d3970351cdd6e4b90cae968230b625
CptFisk <marcus@kammarfelt.eu>
2024-09-12 21:23:27 +0200

    Moved to common from engine

---
+++ include/common/segment.hpp
@@ -0,0 +1,114 @@
+#pragma once
+#include <SDL.h>
+#include <editor/tile.hpp>
+#include <global.hpp>
+#include <graphicsForward.hpp>
+#include <memory>
+#include <vector>
+
+namespace Common {
+
+struct typeSegmentData {
+    std::vector<SDL_Texture*> Layers;   // Each layer contains one animation frame
+    SDL_FRect                 Position; // Position for the frames
+};
+
+struct typeSegment {
+    typeSegmentData Bottom;    // Bottom layer, contains grass, flowers, etc
+    typeSegmentData Top;       // Contains all elements that will be above the player
+    typeSegmentData Lightning; // Contains static lightning effects
+
+    int CurrentLayerBottom{};    // Current layer bottom
+    int MaxLayerBottom{};        // Max layers for bottom graphic
+    int CurrentLayerTop{};       // Current layer top
+    int MaxLayerTop{};           // Max layers for top graphic
+    int CurrentLayerLightning{}; // Current layer for lightning effects
+    int MaxLayerLightning{};     // May layer for lightning effects
+
+    typeSegment()
+      : Bottom{}
+      , Top{}
+      , Lightning{}
+      , CurrentLayerBottom{}
+      , MaxLayerBottom{}
+      , CurrentLayerTop{}
+      , MaxLayerTop{}
+      , CurrentLayerLightning{}
+      , MaxLayerLightning{} {}
+};
+
+void
+createMap();
+
+/**
+ * @brief Create segments that later can be used to generate the bigger picture
+ * @param renderer Reference to the renderer
+ * @param segment Reference to the segment (Bottom, Top, Lightning)
+ * @param animationValue Highest animation value that exist on that layer, for Lightning its a constant
+ * @param maxValue Number of layers for each segment. SAME AS ANIMATION VALUE?
+ */
+void
+createSegments(SDL_Renderer*& renderer, typeSegmentData& segment, const uint8_t& animationValue, int& maxValue);
+
+void
+createMap(SDL_Renderer*&                       renderer,
+          std::shared_ptr<Graphics::Graphics>& graphics,
+          typeSegment&                         segments,
+          const int&                           animationBase,
+          const int&                           animationTop,
+          Level::typeTiles& tiles,
+          Level::typeAssets&                   assets);
+
+/**
+ * @brief Add a smaller texture to the bigger picture
+ * @param renderer Reference to the renderer
+ * @param graphics Reference to the graphics handler
+ * @param segment Reference to the segment (Bottom, Top, Lightning)
+ * @param x X-coordinate
+ * @param y Y-coordinate
+ * @param name Name of the graphic that shall be added
+ */
+void
+addToSegment(SDL_Renderer*&                       renderer,
+             std::shared_ptr<Graphics::Graphics>& graphics,
+             typeSegmentData&                     segment,
+             const int&                           x,
+             const int&                           y,
+             const std::string&                   name);
+
+/**
+ * @brief Clear all data inside a typeSegmentData
+ */
+void
+clearTypeSegmentData(typeSegmentData& data);
+/**
+ * @brief Clear all data inside typeSegment
+ * @param data
+ */
+void
+clearTypeSegment(typeSegment& data);
+
+/**
+ * @Add a alpha channel to a segment and all its layers
+ * @param segment Segment to add
+ * @param blendMode SDL_BlendMode
+ * @param value Alpha value 0-255
+ */
+void
+setSegmentAlpha(typeSegmentData& segment, const SDL_BlendMode& blendMode, const int& value);
+
+/**
+ * @brief Add lightning to a texture
+ * @param renderer Reference to the renderer, later used when calling addSegment
+ * @param graphics Reference to graphic handler, later used when calling addSegment
+ * @param segments Segment that shall receive the lightning effect
+ * @param bitset Bitset for lightning
+ * @param pos Current position
+ */
+void
+addLightning(SDL_Renderer*&                       renderer,
+             std::shared_ptr<Graphics::Graphics>& graphics,
+             typeSegment&                         segments,
+             const std::bitset<32>&               bitset,
+             const int&                           pos);
+}
---
+++ source/common/segment.cpp
@@ -0,0 +1,235 @@
+#include <common/math.hpp>
+#include <common/segment.hpp>
+#include <editor/tile.hpp>
+#include <graphics/graphics.hpp>
+#include <iostream>
+#include <level/types/tile.hpp>
+#include <utility/bits.hpp>
+#include <utility/scale.hpp>
+
+namespace Common {
+
+void
+clearTypeSegmentData(typeSegmentData& data) {
+    for (auto& texture : data.Layers) {
+        if (texture)
+            SDL_DestroyTexture(texture);
+    }
+}
+
+void
+clearTypeSegment(typeSegment& data) {
+    /*
+    for (auto& bottom : data.Bottom)
+        clearTypeSegmentData(bottom);
+    for (auto& top : data.Top)
+        clearTypeSegmentData(top);
+    for (auto& lightning : data.Lightning)
+        clearTypeSegmentData(lightning);
+    data.Bottom.clear();
+    data.Top.clear();
+    data.Lightning.clear();
+    data.MaxLayerBottom     = {};
+    data.CurrentLayerBottom = {};
+    data.MaxLayerTop        = {};
+    data.CurrentLayerTop    = {};
+     */
+}
+
+void
+createMap(SDL_Renderer*&                       renderer,
+          std::shared_ptr<Graphics::Graphics>& graphics,
+          typeSegment&                         segments,
+          const int&                           animationBase,
+          const int&                           animationTop,
+          Level::typeTiles&              tiles,
+          Level::typeAssets&                   assets) {
+    // Create segments
+    Common::createSegments(renderer, segments.Bottom, animationBase, segments.MaxLayerBottom);
+    Common::createSegments(renderer, segments.Top, animationTop, segments.MaxLayerTop);
+    Common::createSegments(renderer, segments.Lightning, LIGHT_ANIMATION_FRAMES, segments.MaxLayerLightning);
+    Common::setSegmentAlpha(segments.Lightning, SDL_BLENDMODE_MUL, 0);
+
+    // Start values
+    int  pos        = 0;
+    bool layersLeft = false;
+    auto it         = tiles.Tiles.begin();
+
+    do {
+        if (it == tiles.Tiles.end()) {
+            it         = tiles.Tiles.begin();
+            pos        = 0;
+            layersLeft = false;
+        }
+        const auto& [x, y] = Common::getCoords(pos, MAP_WIDTH, MAP_WIDTH);
+        auto &tile          = (*it);
+        // Base graphics
+        if ((tile.Type.test(Level::TileType::BASE_TEXTURE) || tile.Type.test(Level::TileType::TOP_TEXTURE)) && !tile.Base.empty()) {
+            const auto id    = tile.Base.front();
+            const auto asset = assets.Assets[id];
+            addToSegment(renderer, graphics, segments.Bottom, x, y, asset);
+            tile.Base.erase(tile.Base.begin()); // Remove the element since it have been displayed
+            if (!tile.Base.empty())
+                layersLeft = true;
+        }
+        // Overlay
+        if (tile.Type.test(Level::TileType::TOP_TEXTURE) && !tile.Top.empty()) {
+            const auto id    = INT(tile.Top.front());
+            const auto asset = assets.Assets[id];
+            addToSegment(renderer, graphics, segments.Top, x, y, asset);
+            tile.Top.erase(tile.Top.begin()); // Remove element
+            if (!tile.Top.empty())
+                layersLeft = true;
+        }
+        pos++;
+    } while (!(++it == tiles.Tiles.end() && !layersLeft));
+    SDL_SetRenderTarget(renderer, nullptr);
+}
+
+void
+createSegments(SDL_Renderer*& renderer, typeSegmentData& segment, const uint8_t& animationValue, int& maxValue) {
+    // We always need at least one layer
+    const auto animationLayers = std::max(static_cast<int>(animationValue), 1);
+
+    std::vector<SDL_Texture*> layers; // All the layers
+    // Generating all layers
+    for (int i = 0; i < animationLayers; i++) {
+        // Create texture
+        auto texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 2048, 2048);
+        /**
+         * It appears that SDL_CreateTexture dont always provide a 100% clean texture, resulting in that we sometime
+         * inherit older parts, we first need to make sure that its cleared.
+         */
+        ASSERT_WITH_MESSAGE(texture == nullptr, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_SetRenderTarget(renderer, texture) != 0, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0) != 0, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_RenderClear(renderer) != 0, SDL_GetError())
+
+        ASSERT_WITH_MESSAGE(SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND) != 0, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_SetTextureAlphaMod(texture, 255) != 0, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_SetRenderTarget(renderer, nullptr) != 0, SDL_GetError())
+        // Creating all layers
+        layers.emplace_back(texture);
+    }
+    segment  = { layers, SDL_FRect{ 0, 0, 2048, 2048 } };
+    maxValue = INT(segment.Layers.size());
+}
+
+void
+addToSegment(SDL_Renderer*&                       renderer,
+             std::shared_ptr<Graphics::Graphics>& graphics,
+             typeSegmentData&                     segment,
+             const int&                           x,
+             const int&                           y,
+             const std::string&                   name) {
+    auto xPos = FLOAT(x * 16.0f);
+    auto yPos = FLOAT(y * 16.0f);
+
+    switch (graphics->getTextureType(name)) {
+        case Graphics::TextureTypes::BaseTexture: {
+            auto            texture     = dynamic_cast<Graphics::BaseTexture*>(graphics->getTexture(name));
+            const SDL_FRect destination = SDL_FRect{ xPos, yPos, FLOAT(texture->getWidth()), FLOAT(texture->getHeight()) };
+            const auto&     viewport    = texture->getRandomViewport();
+            // This is the most basic, apply this texture to all layers
+            for (auto& layer : segment.Layers) {
+                SDL_SetRenderTarget(renderer, layer); // Set render target
+                SDL_RenderCopyF(renderer, texture->getTexture(), &viewport, &destination);
+                ASSERT_WITH_MESSAGE(SDL_SetRenderTarget(renderer, nullptr) != 0, SDL_GetError())
+            }
+
+        } break;
+        case Graphics::TextureTypes::AnimatedTexture: {
+            auto            texture     = dynamic_cast<Graphics::AnimatedTexture*>(graphics->getTexture(name));
+            const SDL_FRect destination = { xPos, yPos, FLOAT(texture->getWidth()), FLOAT(texture->getHeight()) };
+            /**
+             * We start the viewport at 0, then we increment it each time we draw a layer. When we have reached the end of our
+             * viewports we simply restart. Since we calculated the layers to match the lcm of the frames it should all be fine
+             */
+            const auto maxTicks     = texture->getTicks();
+            const auto maxViewports = texture->getViewports().size() - 1;
+            int        tick         = 0;
+            int        viewport     = 0;
+            for (auto layer : segment.Layers) {
+                ASSERT_WITH_MESSAGE(SDL_SetRenderTarget(renderer, layer) != 0, SDL_GetError())
+                SDL_RenderCopyF(renderer, texture->getTexture(), &texture->getViewport(viewport), &destination);
+                ASSERT_WITH_MESSAGE(SDL_SetRenderTarget(renderer, nullptr) != 0, SDL_GetError())
+                if (++tick >= maxTicks) {
+                    tick = 0;
+                    if (viewport++ >= maxViewports)
+                        viewport = 0;
+                }
+            }
+
+        }
+
+        break;
+        case Graphics::TextureTypes::LightningTexture: {
+            auto texture = dynamic_cast<Graphics::LightningTexture*>(graphics->getTexture(name));
+            xPos -= texture->getWidthF() / 2.0f - 8.0f;
+            yPos -= texture->getHeightF() / 2.0f - 8.0f;
+            int viewport = 0;
+            for (auto layer : segment.Layers) {
+                SDL_FRect destination = { xPos, yPos, FLOAT(texture->getWidth()), FLOAT(texture->getHeight()) };
+                SDL_SetRenderTarget(renderer, layer);
+                SDL_RenderCopyF(renderer, texture->getTexture(), &texture->getViewport(viewport++), &destination);
+                SDL_SetRenderTarget(renderer, nullptr);
+            }
+
+        } break;
+        default:
+            std::cerr << "Not supported" << std::endl;
+    }
+    SDL_SetRenderTarget(renderer, nullptr); // Reset renderer
+}
+
+void
+setSegmentAlpha(typeSegmentData& segment, const SDL_BlendMode& blendMode, const int& value) {
+    for (auto& layer : segment.Layers) {
+        ASSERT_WITH_MESSAGE(SDL_SetTextureBlendMode(layer, blendMode) != 0, SDL_GetError())
+        ASSERT_WITH_MESSAGE(SDL_SetTextureAlphaMod(layer, Utility::Scale(value, 0, 100, 0, 255)) != 0, SDL_GetError())
+    }
+}
+
+void
+addLightning(SDL_Renderer*&                       renderer,
+             std::shared_ptr<Graphics::Graphics>& graphics,
+             typeSegment&                         segments,
+             const std::bitset<32>&               bitset,
+             const int&                           pos) {
+    std::string textureName = "Light";
+    switch (Utility::getSetBit(bitset, std::bitset<32>(LIGHT_SHAPE))) {
+        case Level::LIGHT_CIRCLE:
+            textureName += "Circle";
+            break;
+        case Level::LIGHT_SQUARE:
+            textureName += "Square";
+            break;
+        default:
+            return;
+    }
+    switch (Utility::getSetBit(bitset, std::bitset<32>(LIGHT_COLOUR))) {
+        case Level::LIGHT_RED:
+            textureName += "Red";
+            break;
+        case Level::LIGHT_YELLOW:
+            textureName += "Yellow";
+            break;
+        case Level::LIGHT_WHITE:
+            textureName += "White";
+            break;
+        default:
+            return;
+    }
+    switch (Utility::getSetBit(bitset, std::bitset<32>(LIGHT_SIZE))) {
+        case Level::LIGHT_BIG:
+            // addToSegment(renderer, graphics, segments.Lightning, pos, textureName + "Big");
+        case Level::LIGHT_MEDIUM:
+            // addToSegment(renderer, graphics, segments.Lightning, pos, textureName + "Medium");
+        case Level::LIGHT_SMALL:
+            // addToSegment(renderer, graphics, segments.Lightning, pos, textureName + "Small");
+            break;
+        default:
+            return;
+    }
+}
+}
