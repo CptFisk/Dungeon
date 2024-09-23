54f634cd80bacceb8cd956178a5401a8c51d3fb1
CptFisk <marcus@kammarfelt.eu>
2024-09-23 15:28:56 +0200

    Re-work of projectile

--- include/object/projectile.hpp
+++ include/object/projectile.hpp
@@ -1,50 +1,51 @@
 #pragma once
 #include <SDL.h>
 #include <graphics/types/animatedTexture.hpp>
+#include <graphics/types/baseTexture.hpp>
 #include <graphics/types/drawData.hpp>
 #include <memory>
 #include <object/particle.hpp>
 #include <utility>
 
 namespace Objects {
-struct typeProjectileStruct {
-    Graphics::AnimatedTexture* Projectile; // Projectile texture
-    SDL_Texture*               Lightning;  // Lightning texture
-    double                     Angle;      // Travel angle
-    int                        Duration;   // How far until object is destroyed
-    float                      Velocity;   // Velocity of object
-};
 
 class Projectile {
     const int PARTICLE_CHANCE = 3; // Chance that a particle spawn
 
   public:
-    Projectile(const typeProjectileStruct&    setup,
-               const std::pair<float, float>& playerPosition,
-               SDL_Renderer*                  renderer,
+    Projectile(Graphics::AnimatedTexture*       texture,
+               Graphics::BaseTexture*           effect,
+               const SDL_FPoint&                startPosition,
+               const double&                    angle,
+               const int&                       duration,
+               const float&                     velocity,
+               const int&                       damage,
                const std::shared_ptr<Particle>& particle); // Constructor
     ~Projectile();
 
     SDL_FRect* getPosition(); // Returns a pointer to the current position
-
+    [[nodiscard]] bool effectsEnabled() const;
     [[nodiscard]] int                          getNewDuration(); // Calculate and return the new duration
     [[nodiscard]] int                          getDamage() const;
-    [[nodiscard]] Graphics::typeDrawData       getLightning();  // Return draw data for lightning
-    [[nodiscard]] Graphics::typeDrawDataAngled getProjectile(); // Return draw data for the projectile
+    [[nodiscard]] Graphics::typeDrawData       getEffect();                 // Return draw data for lightning
+    [[nodiscard]] Graphics::typeDrawDataAngled getProjectile();             // Return draw data for the projectile
+    [[nodiscard]] SDL_FPoint                   getProjectileCenter() const; // Return the center for a projectile
 
     void move(); // All functions related to movement
   private:
   protected:
-    SDL_Renderer*              pRenderer;          // Reference to renderer
-    Graphics::AnimatedTexture* pProjectile;        // Reference to animated texture
-    SDL_Texture*               pLightning;         // Reference to the lightning, if it exists
-    SDL_FRect                  mCurrentPosition;   // Current position of the projectile
-    SDL_FRect                  mLightningPosition; // Current position of light (if used)
+    const bool mParticleEnabled; // If particle was enabled
+    const bool mEffectEnabled;
+
+    Graphics::AnimatedTexture* pProjectile;       // Reference to animated texture
+    Graphics::BaseTexture*     pEffect;           // Reference to the lightning, if it exists
+    SDL_FRect                  mParticlePosition; // Current position of the projectile
+    SDL_FPoint                 mParticleCenter;   // Center of particles
+    SDL_FRect                  mEffectPosition;   // Current position of effect (if used)
     int                        mDamage;
-    double                     mAngle;           // Rotation angle
-    const float                mVelocity;        // Velocity
-    int                        mDuration;        // Number of ticks, destroyed on 0
-    std::shared_ptr<Particle>  mParticle;        // Reference to the particle engine
-    bool                       mParticleEnabled; // If particle was enabled
+    double                     mAngle;    // Rotation angle
+    const float                mVelocity; // Velocity
+    int                        mDuration; // Number of ticks, destroyed on 0
+    std::shared_ptr<Particle>  mParticle; // Reference to the particle engine
 };
 }
--- source/object/projectile.cpp
+++ source/object/projectile.cpp
@@ -3,53 +3,75 @@
 
 namespace Objects {
 
-Projectile::Projectile(const Objects::typeProjectileStruct& setup,
-                       const std::pair<float, float>&       playerPosition,
-                       SDL_Renderer*                        renderer,
-                       const std::shared_ptr<Particle>&     particle)
-  : pProjectile(setup.Projectile)
+Projectile::Projectile(Graphics::AnimatedTexture*       texture,
+                       Graphics::BaseTexture*           effect,
+                       const SDL_FPoint&                startPosition,
+                       const double&                    angle,
+                       const int&                       duration,
+                       const float&                     velocity,
+                       const int&                       damage,
+                       const std::shared_ptr<Particle>& particle)
+  : pProjectile(texture)
   , mParticle(particle)
-  , pLightning(setup.Lightning)
-  , pRenderer(renderer)
-  , mDuration(setup.Duration)
-  , mVelocity(setup.Velocity)
-  , mAngle(setup.Angle)
-  , mDamage(50)
-  , mCurrentPosition{ playerPosition.first + (4.0f), playerPosition.second + (4.0f), 16.0f, 16.0f }
-  , mLightningPosition{ playerPosition.first + ((8.0f / 2.0f) - (16.0f / 2.0f)),
-                        playerPosition.second + ((8.0f / 2.0f) - (16.0f / 2.0f)),
-                        16.0f,
-                        16.0f } {
-    mParticleEnabled = particle != nullptr ? true : false;
-}
+  , pEffect(effect)
+  , mDuration(duration)
+  , mVelocity(velocity)
+  , mAngle(angle)
+  , mDamage(damage)
+  , mParticleCenter(startPosition)
+  , mParticleEnabled(particle != nullptr ? true : false)
+  , mEffectEnabled(effect != nullptr ? true : false)
+  // Set up the position dynamically
+  , mParticlePosition{ startPosition.x - (texture->getWidthF() / 2.0f),
+                       startPosition.y - (texture->getHeightF() / 2.0f),
+                       texture->getWidthF(),
+                       texture->getHeightF() }
+  , mEffectPosition{ startPosition.x + ((8.0f / 2.0f) - (16.0f / 2.0f)),
+                     startPosition.y + ((8.0f / 2.0f) - (16.0f / 2.0f)),
+                     16.0f,
+                     16.0f } {}
 
 Projectile::~Projectile() = default;
 
 SDL_FRect*
 Projectile::getPosition() {
-    return &mCurrentPosition;
+    return &mParticlePosition;
 }
 
 Graphics::typeDrawData
-Projectile::getLightning() {
-    return Graphics::typeDrawData{ pLightning, nullptr, &mLightningPosition };
+Projectile::getEffect() {
+    return Graphics::typeDrawData{ pEffect->getTexture(), nullptr, &mEffectPosition };
 }
 
 Graphics::typeDrawDataAngled
 Projectile::getProjectile() {
     if (mParticleEnabled && rand() % PARTICLE_CHANCE == 1)
-        mParticle->addParticle(mCurrentPosition);
-    return Graphics::typeDrawDataAngled{ pProjectile->getTexture(), pProjectile->getAnimatedViewport(), &mCurrentPosition, mAngle };
+        mParticle->addParticle(mParticlePosition);
+    return Graphics::typeDrawDataAngled{ pProjectile->getTexture(), pProjectile->getAnimatedViewport(), &mParticlePosition, mAngle };
 };
+
+SDL_FPoint
+Projectile::getProjectileCenter() const {
+    return mParticleCenter;
+}
 
 void
 Projectile::move() {
     // Move to new position
     auto delta = Utility::calculateVector(mAngle, mVelocity);
-    mCurrentPosition.x += delta.x;
-    mCurrentPosition.y += delta.y;
-    mLightningPosition.x += delta.x;
-    mLightningPosition.y += delta.y;
+    mParticlePosition.x += delta.x;
+    mParticlePosition.y += delta.y;
+
+    mParticleCenter.x += delta.x;
+    mParticleCenter.y += delta.y;
+
+    mEffectPosition.x += delta.x;
+    mEffectPosition.y += delta.y;
+}
+
+bool
+Projectile::effectsEnabled() const {
+    return mEffectEnabled;
 }
 
 int
