5c567f92c574bd99ef194c80ee27c61ae2137523
CptFisk <marcus@kammarfelt.eu>
2024-09-24 22:06:17 +0200

    Function changes

--- include/utility/textures.hpp
+++ include/utility/textures.hpp
@@ -1,23 +1,26 @@
 #pragma once
 #include <SDL.h>
-#include <utility>
 #include <global.hpp>
+#include <utility>
 #include <vector>
 
-namespace Utility{
+namespace Utility {
 /**
  * @brief Return the center position of a SDL_FRect
  * @param rect Rectangle
  * @return
  */
-std::pair<float,float> getFRectCenter(const SDL_FRect& rect);
+std::pair<float, float>
+getFRectCenter(const SDL_FRect& rect);
 
 /**
  * @brief Return true if objects overlap
  * @return
  */
-bool isOverlapping(const SDL_FRect& obj1, const SDL_FRect& obj2);
-bool isOverlapping(const SDL_FPoint& obj1, const SDL_FRect& obj2);
+bool
+isOverlapping(const SDL_FRect& obj1, const SDL_FRect& obj2);
+bool
+isOverlapping(const SDL_FPoint& obj1, const SDL_FRect& obj2);
 
 /**
  * @brief Return true if 2 SDL_FRect is about to collide in a certain direction
@@ -38,5 +41,6 @@ isColliding(const SDL_FRect& rect1, const SDL_FRect& rect2, Directions direction
 bool
 isAdjacent(const SDL_FRect& rect1, const SDL_FRect& rect2);
 
-std::vector<SDL_FRect> optimizeSDL_FRect(std::vector<SDL_FRect> obj);
+std::vector<SDL_FRect>
+optimizeSDL_FRect(std::vector<SDL_FRect> obj);
 }
