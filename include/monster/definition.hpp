bd509cf284bd884f2e4e20d3a1c0343f2c49d9e9
CptFisk <marcus@kammarfelt.eu>
2024-09-29 21:53:34 +0200

    Added beanmean flower

--- include/monster/definition.hpp
+++ include/monster/definition.hpp
@@ -10,22 +10,26 @@ struct MonsterData {
 };
 
 enum class Type {
-    SPARE1       = 1, // Spare
-    CAVE_CRAWLER = 2, // Cave crawler
-    SLIME_BLUE   = 3, // Blue small slime
-    SLIME_GREEN  = 4, // Green small slime
-    SLIME_PURPLE = 5, // Purple small slime
-    SLIME_RED    = 6, // Red small slime
-    BEAN_MAN     = 7  // Green bean man
+    SPARE1          = 1, // Spare
+    CAVE_CRAWLER    = 2, // Cave crawler
+    SLIME_BLUE      = 3, // Blue small slime
+    SLIME_GREEN     = 4, // Green small slime
+    SLIME_PURPLE    = 5, // Purple small slime
+    SLIME_RED       = 6, // Red small slime
+    BEAN_MAN        = 7, // Green bean man
+    BEAN_MAN_FLOWER = 8  // Green bean man with a flower
 };
 
-const std::array<MonsterData, 8> monsters = { { { "", "" },                             // Default blank
-                                                { "SPARE1", "SPARE" },                  // Slime
-                                                { "Cave Crawler", "CaveCrawlerSouth" }, // Cave Crawler
-                                                { "Slime blue", "SlimeBlueSouth" },     // Blue slime
-                                                { "Slime green", "SlimeGreenSouth" },   // Green slime
-                                                { "Slime purple", "SlimePurpleSouth" }, // Purple slime
-                                                { "Slime red", "SlimeRedSouth" },       // Red slime
-                                                { "Bean man", "BeanManSouth" } } };
+const std::array<MonsterData, 9> monsters = { {
+  { "", "" },                                 // Default blank
+  { "SPARE1", "SPARE" },                      // Slime
+  { "Cave Crawler", "CaveCrawlerSouth" },     // Cave Crawler
+  { "Slime blue", "SlimeBlueSouth" },         // Blue slime
+  { "Slime green", "SlimeGreenSouth" },       // Green slime
+  { "Slime purple", "SlimePurpleSouth" },     // Purple slime
+  { "Slime red", "SlimeRedSouth" },           // Red slime
+  { "Bean man", "BeanManSouth" },             // Normal bean man
+  { "Bean man flower", "BeanManFlowerSouth" } // Bean man with a flower
+} };
 
 }
