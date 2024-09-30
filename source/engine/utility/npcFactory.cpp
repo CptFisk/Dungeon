193971b10dfbd20a1018e789aefe0d9515e935cd
CptFisk <marcus@kammarfelt.eu>
2024-09-30 21:12:40 +0200

    Attempt of a npc factory

--- source/engine/utility/npcFactory.cpp
+++ source/engine/utility/npcFactory.cpp
@@ -4,6 +4,14 @@ namespace Engine {
 
 void
 Engine::createNPC() {
-
+    mNPCs[NPC::Type::COW] = new NPC::Cow([&](const SDL_FPoint& point, const float& x, const float& y) { return movementWalls(point, x, y); });
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::MOVE, Directions::NORTH, GET_ANIMATED("Cow1North"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::MOVE, Directions::EAST, GET_ANIMATED("Cow1East"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::MOVE, Directions::SOUTH, GET_ANIMATED("Cow1South"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::MOVE, Directions::WEST, GET_ANIMATED("Cow1West"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::IDLE, Directions::NORTH, GET_ANIMATED("Cow1NorthIdle"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::IDLE, Directions::EAST, GET_ANIMATED("Cow1EastIdle"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::IDLE, Directions::SOUTH, GET_ANIMATED("Cow1SouthIdle"));
+    mNPCs[NPC::Type::COW]->addAnimatedTexture(Objects::IDLE, Directions::WEST, GET_ANIMATED("Cow1WestIdle"));
 }
 }
