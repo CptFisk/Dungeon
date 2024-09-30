bf8d85f09c33289f43ab466fcc2c6c105fe78284
CptFisk <marcus@kammarfelt.eu>
2024-09-30 08:50:00 +0200

    Re-order

--- include/monster/include.hpp
+++ include/monster/include.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <monster/beanMan.hpp>
 #include <monster/caveCrawler.hpp>
 #include <monster/definition.hpp>
 #include <monster/monster.hpp>
 #include <monster/slime.hpp>
-#include <monster/beanMan.hpp>
