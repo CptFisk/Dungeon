c6c71ea0950809e994110247407c6437467ac695
CptFisk <marcus@kammarfelt.eu>
2024-11-26 07:48:43 +0100

    Removed boilerplate

--- include/object/objects.hpp
+++ include/object/objects.hpp
@@ -1,15 +1,15 @@
 #pragma once
 #include <nlohmann/json.hpp>
 namespace Objects {
-#define OBJECT_ACTION(DO) \
-    DO(IDLE)              \
-    DO(MOVE)              \
-    DO(ATTACK)            \
-    DO(DYING)             \
-    DO(DEAD)
 
-#define MAKE_OBJECT_ACTION(VAR) VAR,
-enum State { OBJECT_ACTION(MAKE_OBJECT_ACTION) };
+enum State{
+    IDLE = 0,
+    MOVE = 1,
+    ATTACK = 2,
+    DYING = 3,
+    DEAD = 4
+};
+
 NLOHMANN_JSON_SERIALIZE_ENUM(State, { { IDLE, "Idle" }, { MOVE, "MOVE" }, { ATTACK, "ATTACK" } })
 
 }
