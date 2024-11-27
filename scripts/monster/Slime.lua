b3cdf18b51b1acbfd242a0768247994ab0694262
CptFisk <marcus@kammarfelt.eu>
2024-11-27 10:29:00 +0100

    Changes to movement

--- scripts/monster/Slime.lua
+++ scripts/monster/Slime.lua
@@ -1,5 +1,13 @@
 function Interact(monster)
     local state = monster:GetState()
-    print(state)
-
+    if(state == ObjectState.IDLE) then
+        --Calculate distances
+        local mx,my = monster:GetCenter()
+        local px,py = GetPlayerCenter()
+        local dist = GetDistance(mx,my,px,py)
+        if(dist > 8) then
+            monster:SetState(ObjectState.MOVE)
+        end
+        print(dist)
+    end
 end
