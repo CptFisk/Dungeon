66b280fb22214cd7e4a59363ba095c8f419c3889
CptFisk <marcus@kammarfelt.eu>
2024-12-17 14:33:59 +0100

    Splitted to multiple files

---
+++ scripts/monster/SlimePurple.lua
@@ -0,0 +1,28 @@
+function Interact(monster)
+    local state = monster:GetState()
+
+    if(state == ObjectState.IDLE) then
+
+        --Calculate distances
+        local mx,my = monster:GetCenter()
+        local px,py = GetPlayerCenter()
+        local dist = GetDistance(mx,my,px,py)
+        if(dist <= 8) then
+            monster:SetState(ObjectState.MOVE)
+        end
+    elseif state == ObjectState.MOVE then
+        --Calculate distances
+        local mx,my = monster:GetCenter()
+        local px,py = GetPlayerCenter()
+        local dist = GetDistance(mx,my,px,py)
+        if(dist > 8) then
+            monster:SetState(ObjectState.IDLE)
+        else
+            local angle = GetAngle(px,py, mx,my)
+            local x,y = GetVector(angle, monster:GetVelocity())
+            if(CheckObstacle(mx+x, my+y)) then
+                monster:MoveAngle(angle, monster:GetVelocity())
+            end
+        end
+    end
+end
