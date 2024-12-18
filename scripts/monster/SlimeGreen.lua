ad320512131e61de3bbd5cfe991c53d9875bcd49
CptFisk <marcus@kammarfelt.eu>
2024-12-18 14:27:18 +0100

    Added radius from retain memory

--- scripts/monster/SlimeGreen.lua
+++ scripts/monster/SlimeGreen.lua
@@ -1,13 +1,14 @@
 function Interact(monster)
     local state = monster:GetState()
-
+    local radius = monster:GetRetain("Radius")
     if(state == ObjectState.IDLE) then
 
         --Calculate distances
         local mx,my = monster:GetCenter()
         local px,py = GetPlayerCenter()
         local dist = GetDistance(mx,my,px,py)
-        if(dist <= 8) then
+
+        if(dist <= radius) then
             monster:SetState(ObjectState.MOVE)
         end
     elseif state == ObjectState.MOVE then
@@ -15,7 +16,7 @@ function Interact(monster)
         local mx,my = monster:GetCenter()
         local px,py = GetPlayerCenter()
         local dist = GetDistance(mx,my,px,py)
-        if(dist > 8) then
+        if(dist > radius) then
             monster:SetState(ObjectState.IDLE)
         else
             local angle = GetAngle(px,py, mx,my)
