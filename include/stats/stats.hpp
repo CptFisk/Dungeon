ad561582edd9c0e613afa25ff3e5bdbaff84f218
CptFisk <marcus@kammarfelt.eu>
2024-12-21 21:29:01 +0100

    Basic stats

---
+++ include/stats/stats.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Stats {
+struct Stats {
+    int Vitality;     // Determines the player health
+    int Strength;     // Determines the strength
+    int Dexterity;    // Dexterity
+    int Intelligence; // Improves casting
+    int Luck;         // Random parameters
+
+    //Basic constructor
+    Stats()
+      : Vitality(0)
+      , Strength(0)
+      , Dexterity(0)
+      , Intelligence(0)
+      , Luck(0) {}
+};
+}
