9e3eb38fab8c2c9fce23a18775719ea2ed00d70d
CptFisk <marcus@kammarfelt.eu>
2024-12-21 22:09:12 +0100

    Creating JSON parser

--- include/stats/stats.hpp
+++ include/stats/stats.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <nlohmann/json.hpp>
 
 namespace Stats {
+
 struct Stats {
     int Vitality;     // Determines the player health
     int Stamina;      // Stamina
@@ -18,4 +20,55 @@ struct Stats {
       , Intelligence(0)
       , Luck(0) {}
 };
+
+inline void
+to_json(nlohmann::json& nlohmann_json_j, const Stats& nlohmann_json_t) {
+    nlohmann_json_j["Vitality"]     = nlohmann_json_t.Vitality;
+    nlohmann_json_j["Stamina"]      = nlohmann_json_t.Stamina;
+    nlohmann_json_j["Strength"]     = nlohmann_json_t.Strength;
+    nlohmann_json_j["Dexterity"]    = nlohmann_json_t.Dexterity;
+    nlohmann_json_j["Intelligence"] = nlohmann_json_t.Intelligence;
+    nlohmann_json_j["Luck"]         = nlohmann_json_t.Luck;
+}
+
+inline void
+from_json(const nlohmann::json& nlohmann_json_j, Stats& nlohmann_json_t) {
+    // Vitality
+    if (nlohmann_json_j.contains("Vitality")) {
+        nlohmann_json_t.Vitality = nlohmann_json_j.at("Vitality").get<int>();
+    } else {
+        nlohmann_json_t.Vitality = 0;
+    }
+    // Stamina
+    if (nlohmann_json_j.contains("Stamina")) {
+        nlohmann_json_t.Stamina = nlohmann_json_j.at("Stamina").get<int>();
+    } else {
+        nlohmann_json_t.Stamina = 0;
+    }
+    // Strength
+    if (nlohmann_json_j.contains("Strength")) {
+        nlohmann_json_t.Strength = nlohmann_json_j.at("Strength").get<int>();
+    } else {
+        nlohmann_json_t.Strength = 0;
+    }
+    // Dexterity
+    if (nlohmann_json_j.contains("Dexterity")) {
+        nlohmann_json_t.Dexterity = nlohmann_json_j.at("Dexterity").get<int>();
+    } else {
+        nlohmann_json_t.Dexterity = 0;
+    }
+    // Intelligence
+    if (nlohmann_json_j.contains("Intelligence")) {
+        nlohmann_json_t.Intelligence = nlohmann_json_j.at("Intelligence").get<int>();
+    } else {
+        nlohmann_json_t.Intelligence = 0;
+    }
+    // Luck
+    if (nlohmann_json_j.contains("Luck")) {
+        nlohmann_json_t.Luck = nlohmann_json_j.at("Luck").get<int>();
+    } else {
+        nlohmann_json_t.Luck = 0;
+    }
+}
+
 }
