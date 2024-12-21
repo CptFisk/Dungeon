7b3fdf501071022b1d557e899c12d3d36f2ad914
CptFisk <marcus@kammarfelt.eu>
2024-12-21 22:10:50 +0100

    Added JSON

--- include/items/slots.hpp
+++ include/items/slots.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <cstdint>
-
+#include <nlohmann/json.hpp>
 namespace Items {
 enum class SlotType {
     Undefined, // Not defined
@@ -13,5 +13,16 @@ enum class SlotType {
     Bag,   // Might be set later
     Spell
 };
+
+NLOHMANN_JSON_SERIALIZE_ENUM(SlotType,
+                             { { SlotType::Undefined, "Undefined" },
+                               { SlotType::Amulet, "Amulet" },
+                               { SlotType::Head, "Head" },
+                               { SlotType::Chest, "Chest" },
+                               { SlotType::Right, "Right" },
+                               { SlotType::Left, "Left" },
+                               { SlotType::Boots, "Boots" },
+                               { SlotType::Bag, "Bag" },
+                               { SlotType::Spell, "Spell" }})
 
 }
