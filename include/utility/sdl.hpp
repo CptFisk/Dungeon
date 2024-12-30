263182d28a12c2304abeebfc85c63b556f9f4369
CptFisk <marcus@kammarfelt.eu>
2024-12-30 23:07:09 +0100

    New functions

--- include/utility/sdl.hpp
+++ include/utility/sdl.hpp
@@ -6,8 +6,18 @@ inline constexpr SDL_Point FPointToPoint(const SDL_FPoint& point){
     return SDL_Point{static_cast<int>(point.x), static_cast<int>(point.y)};
 }
 
+inline constexpr SDL_Point addPoint(const SDL_Point& a, const SDL_Point& b){
+    return {a.x + b.x, a.y + b.y};
+}
+
 inline constexpr SDL_FPoint PointToFPoint(const SDL_Point& point){
     return SDL_FPoint {static_cast<float>(point.x), static_cast<float>(point.y)};
 }
+
+inline constexpr SDL_FPoint addFPoint(const SDL_FPoint& a, const SDL_FPoint& b){
+    return {a.x + b.x, a.y + b.y};
+}
+
+
 
 }
