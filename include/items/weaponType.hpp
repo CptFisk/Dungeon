97122339fe3c98f0e5cee826ba980ddc19d967da
CptFisk <marcus@kammarfelt.eu>
2025-01-01 22:42:59 +0100

    Added default weapon type

--- include/items/weaponType.hpp
+++ include/items/weaponType.hpp
@@ -3,10 +3,11 @@
 
 namespace Items {
 
-enum class WeaponType { Staff, Axe, Sword, Bow, Shield };
+enum class WeaponType { None, Staff, Axe, Sword, Bow, Shield };
 
 NLOHMANN_JSON_SERIALIZE_ENUM(WeaponType,
-                             { { WeaponType::Staff, "Staff" },
+                             { { WeaponType::None, "None" },
+                               { WeaponType::Staff, "Staff" },
                                { WeaponType::Axe, "Axe" },
                                { WeaponType::Sword, "Sword" },
                                { WeaponType::Bow, "Bow" },
