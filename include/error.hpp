b65f03c010d92f0fea395f91b0d748f4f4ec2618
CptFisk <marcus@kammarfelt.eu>
2025-01-06 20:11:04 +0100

    Fixed clang errors

--- include/error.hpp
+++ include/error.hpp
@@ -1,10 +1,10 @@
 #pragma once
-#include <assert.h>
+#include <cassert>
 #include <iostream>
 
 // Custom assert macro with a message
 #define ASSERT_WITH_MESSAGE(condition, message)                                                       \
     if (condition) {                                                                                  \
-        std::cerr << #condition << " " << __FILE__ << " " << __LINE__ << " " << message << std::endl; \
+        std::cerr << #condition << " " << __FILE__ << " " << __LINE__ << " " << (message) << std::endl; \
         std::abort();                                                                                 \
     }
