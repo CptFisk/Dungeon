272fa63b7636a5b58a63dc490e720eb2c30ec55f
CptFisk <marcus@kammarfelt.eu>
2025-01-07 20:09:45 +0100

    Clang errors

--- include/error.hpp
+++ include/error.hpp
@@ -5,6 +5,6 @@
 // Custom assert macro with a message
 #define ASSERT_WITH_MESSAGE(condition, message)                                                       \
     if (condition) {                                                                                  \
-        std::cerr << #condition << " " << __FILE__ << " " << __LINE__ << " " << (message) << std::endl; \
+        std::cerr << #condition << " " << __FILE__ << " " << __LINE__ << " " << message << std::endl; \
         std::abort();                                                                                 \
     }
