3103651a543a0817fe1dff578497387485e36eab
CptFisk <marcus@kammarfelt.eu>
2025-01-11 23:27:17 +0100

    Added more values to scaling

--- include/common/scale.hpp
+++ include/common/scale.hpp
@@ -8,13 +8,17 @@ namespace Common {
  * @brief a grid size of 16x12 squares.
  */
 struct typeScale {
-    float factorX;       // Value used to scale the width of each level, 1 means 16 squares width
-    float factorY;       // Value used to scale the height of each level, 1 means 16 squares width
-    int   windowWidth;   // Window width
-    float windowWidthF;  // Window width (float)
-    int   windowHeight;  // Window Height
-    float windowHeightF; // Window width (float)
-    float selectedScale; // Scale that we stick with
+    float factorX;             // Value used to scale the width of each level, 1 means 16 squares width
+    float factorY;             // Value used to scale the height of each level, 1 means 16 squares width
+    int   windowWidth;         // Window width
+    float windowWidthF;        // Window width (float)
+    int   windowHeight;        // Window Height
+    float windowHeightF;       // Window width (float)
+    int   scaledWindowWidth;    // Width of scaled window
+    float scaledWindowWidthF;  // Width of scaled window (float)
+    int   scaledWindowHeight;  // Height of scaled window
+    float scaledWindowHeightF; // Height of scaled window (float)
+    float selectedScale;       // Scale that we stick with
     typeScale()
       : factorX(0.0f)
       , factorY(0.0f)
@@ -22,6 +26,10 @@ struct typeScale {
       , windowHeight(0)
       , windowWidthF(0.0f)
       , windowHeightF(0.0f)
+      , scaledWindowHeight(0)
+      , scaledWindowHeightF(0.0f)
+      , scaledWindowWidth(0)
+      , scaledWindowWidthF(0.0f)
       , selectedScale(0.0f) {}
 };
 
--- source/common/scale.cpp
+++ source/common/scale.cpp
@@ -8,16 +8,21 @@ calculateGameScale(typeScale& scale, SDL_Window* window) {
     float factorX, factorY; // Internal variables
     SDL_GetWindowSizeInPixels(window, &width, &height);
     const auto squaresX  = static_cast<float>(BLOCKS_WIDTH);  // Numbers of square in x-direction
-    const auto squareY   = static_cast<float>(BLOCKS_HEIGHT); // Numbers of square in y-direction
+    const auto squaresY  = static_cast<float>(BLOCKS_HEIGHT); // Numbers of square in y-direction
     const auto pixelSize = static_cast<float>(PIXEL_SIZE);
     factorX              = (static_cast<float>(width) / squaresX) / pixelSize;
-    factorY              = (static_cast<float>(height) / squareY) / pixelSize;
+    factorY              = (static_cast<float>(height) / squaresY) / pixelSize;
     scale.windowWidth    = width;
     scale.windowWidthF   = static_cast<float>(width);
     scale.windowHeight   = height;
     scale.windowHeightF  = static_cast<float>(height);
 
     scale.selectedScale = std::ceil(std::min(factorX, factorY)); // Selecting the smallest value
+    // Window dimensions
+    scale.scaledWindowWidthF  = static_cast<float>(width) / static_cast<float>(scale.selectedScale);
+    scale.scaledWindowWidth   = static_cast<int>(scale.scaledWindowWidthF);
+    scale.scaledWindowHeightF = static_cast<float>(height) / static_cast<float>(scale.selectedScale);
+    scale.scaledWindowHeight  = static_cast<int>(scale.scaledWindowHeightF);
 
     if (factorX == factorY) {
         scale.factorX = 1.0f;
